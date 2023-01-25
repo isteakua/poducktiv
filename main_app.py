4bc8d926475d2eff2e4c7c874f59bc21602bbc18
Isteak <121730770+istteak@users.noreply.github.com>
2023-01-25 12:46:44 +0600

    Start the project Poducktiv

--- main_app.py
+++
@@ -1,9 +0,0 @@
-from kivy.app import App
-from kivy.uix.label import Label
-
-class PoducktivApp(App):
-    def build(self):
-        label = Label(text="Hello, user!")
-        return label
-
-PoducktivApp().run()
